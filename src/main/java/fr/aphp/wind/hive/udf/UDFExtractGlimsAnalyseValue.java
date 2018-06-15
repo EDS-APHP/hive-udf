f103819490a8be1b5b1f8777b025e09f5de26dee
Nicolas Paris <nicolas.paris@aphp.fr>
2018-06-15 21:11:30 +0200

    Add gp_text handling

--- src/main/java/fr/aphp/wind/hive/udf/UDFExtractGlimsAnalyseValue.java
+++ src/main/java/fr/aphp/wind/hive/udf/UDFExtractGlimsAnalyseValue.java
@@ -1,10 +1,15 @@
 package fr.aphp.wind.hive.udf;
 
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.apache.derby.impl.sql.catalog.SYSROUTINEPERMSRowFactory;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
 import org.apache.hadoop.hive.ql.udf.generic.GenericUDTF;
@@ -14,16 +19,25 @@ import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorFactory;
 import org.apache.hadoop.hive.serde2.objectinspector.StructObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.StringObjectInspector;
+import com.univocity.parsers.csv.CsvParser;
+import com.univocity.parsers.csv.CsvParserSettings;
 
 public class UDFExtractGlimsAnalyseValue extends GenericUDTF {
 
-  private static final Integer OUT_COLS = 4;
+  private static final Integer OUT_COLS = 8;
   // the output columns size
   private transient Object forwardColObj[] = new Object[OUT_COLS];
 
   private transient ObjectInspector[] inputOIs;
   private Pattern typeNumericPattern;
   private Pattern typeDatetimePattern;
+  private Pattern typeImagePattern;
+  private Pattern typeDatePattern;
+  private Pattern typeHourPattern;
+  private Pattern typeXmlPattern;
+  private Pattern typeGptextPattern;
+
+  private HashMap<String, String> map;
 
   /**
    *
@@ -34,76 +48,326 @@ public class UDFExtractGlimsAnalyseValue extends GenericUDTF {
   @Override
   public StructObjectInspector initialize(ObjectInspector[] argOIs) throws UDFArgumentException {
 
-    if (argOIs.length != 2 || argOIs[0].getCategory() != ObjectInspector.Category.PRIMITIVE
-        || !argOIs[0].getTypeName().equals(serdeConstants.STRING_TYPE_NAME)) {
+    if (argOIs.length != 3
+        || (argOIs[0] != null && (argOIs[0].getCategory() != ObjectInspector.Category.PRIMITIVE
+            || !argOIs[0].getTypeName().equals(serdeConstants.STRING_TYPE_NAME)))) {
       throw new UDFArgumentException("split_url only take one argument with type of string");
     }
 
     inputOIs = argOIs;
     List<String> outFieldNames = new ArrayList<String>();
     List<ObjectInspector> outFieldOIs = new ArrayList<ObjectInspector>();
-    
-    outFieldNames.add("type_calc");
+
+    outFieldNames.add("value_type_calc");
+    outFieldOIs.add(PrimitiveObjectInspectorFactory.javaStringObjectInspector);
     outFieldNames.add("value_num_calc");
-    outFieldNames.add("unit_calc");
-    outFieldNames.add("value_text_calc");
-    
-    outFieldOIs.add(PrimitiveObjectInspectorFactory.javaStringObjectInspector);
     outFieldOIs.add(PrimitiveObjectInspectorFactory.javaDoubleObjectInspector);
-    // writableStringObjectInspector correspond to hadoop.io.Text
+    outFieldNames.add("value_text_calc");
     outFieldOIs.add(PrimitiveObjectInspectorFactory.javaStringObjectInspector);
+    outFieldNames.add("value_num_unit_calc");
     outFieldOIs.add(PrimitiveObjectInspectorFactory.javaStringObjectInspector);
-   
-    
-     typeNumericPattern = Pattern.compile("^[<> -]*\\d{1,8}\\.?\\d{0,5}");
-     typeDatetimePattern = Pattern.compile("^[0-9]{1,2}/[0-9]{1,2}/[0-9]{1,4} [0-9]{2}:[0-9]{2}$");
-     
+    outFieldNames.add("value_num_operator_calc");
+    outFieldOIs.add(PrimitiveObjectInspectorFactory.javaStringObjectInspector);
+    outFieldNames.add("value_num_borne_inf_calc");
+    outFieldOIs.add(PrimitiveObjectInspectorFactory.javaDoubleObjectInspector);
+    outFieldNames.add("value_num_borne_sup_calc");
+    outFieldOIs.add(PrimitiveObjectInspectorFactory.javaDoubleObjectInspector);
+    outFieldNames.add("value_num_borne_calc");
+    outFieldOIs.add(PrimitiveObjectInspectorFactory.javaStringObjectInspector);
+
+
+
+    typeNumericPattern = Pattern.compile("^[<> ]*([-]?\\d{1,8}\\.?\\d{0,5}) ([^\\d]+)$");
+    typeDatetimePattern = Pattern.compile("^[0-9]{1,2}/[0-9]{1,2}/[0-9]{1,4} [0-9]{2}:[0-9]{2}$");
+    typeImagePattern = Pattern.compile("(?i)(?:\\.png$)|(?:\\.tif$)|(?:\\.bmp$)");
+    typeDatePattern = Pattern.compile("^[0-9]{1,2}/[0-9]{1,2}/[0-9]{1,4}$");
+    typeHourPattern = Pattern.compile("^[0-9]{2}h[0-9]{2}min$");
+    typeXmlPattern = Pattern.compile("<\\?xml");
+    typeGptextPattern = Pattern.compile("\\{<[\\+]?([\\w_]+?)\\}");
+
+    map = new HashMap<String, String>();
+    populateMap("glims_ref_gp_text.csv");
+
 
     return ObjectInspectorFactory.getStandardStructObjectInspector(outFieldNames, outFieldOIs);
   }
 
-  final
+  public Reader getReader(String relativePath) {
+
+    try {
+
+      return new FileReader(relativePath);
+    } catch (FileNotFoundException e) {
+      throw new RuntimeException(e);
+    }
+
+  }
+
+  private void populateMap(String path) {
+
+    CsvParserSettings parserSettings = new CsvParserSettings();
+    // the file used in the example uses '\n' as the line separator sequence.
+    // the line separator sequence is defined here to ensure systems such as MacOS and Windows
+    // are able to process this file correctly (MacOS uses '\r'; and Windows uses '\r\n').
+    parserSettings.getFormat().setLineSeparator("\n");
+    parserSettings.setHeaderExtractionEnabled(false);
+    parserSettings.setQuoteDetectionEnabled(true);
+    parserSettings.getFormat().setCharToEscapeQuoteEscaping('"');
+    parserSettings.getFormat().setDelimiter(",".charAt(0));
+    parserSettings.setMaxCharsPerColumn(-1);
+
+    // creates a CSV parser
+    CsvParser parser = new CsvParser(parserSettings);
+    parser.beginParsing(getReader(path));
+
+    String[] row;
+    while ((row = parser.parseNext()) != null) {
+      map.put(row[0].toLowerCase(), row[1]);
+    }
+
+    // The resources are closed automatically when the end of the input is reached,
+    // or when an error happens, but you can call stopParsing() at any time.
+
+    // You only need to use this if you are not parsing the entire content.
+    // But it doesn't hurt if you call it anyway.
+    parser.stopParsing();
+
+
+  }
 
   private String inferType(String value) {
-    
+
+    if (value == null) {
+      return "text";
+    }
+    if (typeGptextPattern.matcher(value).find()) {
+      return "gp_text";
+    }
+    if (typeImagePattern.matcher(value).find()) {
+      return "image";
+    }
     if (typeDatetimePattern.matcher(value).find()) {
       return "datetime";
     }
-    if (typeNumericPattern.matcher(value).find()) {
+    if (typeDatePattern.matcher(value).find()) {
+      return "date";
+    }
+    if (typeHourPattern.matcher(value).find()) {
+      return "hour";
+    }
+    if (typeNumericPattern.matcher(value).matches()) {
       return "numeric";
+    }
+    if (typeXmlPattern.matcher(value).find()) {
+      return "xml";
     }
 
     return "text";
   }
 
+
+
+  public String populateGptext(String s, String source) {
+    if (s == null) {
+      return null;
+    }
+
+    Matcher m = this.typeGptextPattern.matcher(s);
+    LinkedList<String> ret = new LinkedList<String>();
+    int previousBegin = 0;
+    while (m.find()) {
+      ret.add(s.substring(previousBegin, m.start()));
+      ret.add(fetchGptext(m, source));
+      previousBegin = m.end();
+    }
+    if (previousBegin != s.length()) {
+      ret.add(s.substring(previousBegin, s.length()));
+    }
+
+    return String.join("", ret);
+  }
+
+  private String fetchGptext(Matcher match, String source) {
+    String value = source + match.group(1).toLowerCase();
+    String key = map.get(value);
+    return key;
+  }
+
+  private Double[] explodeRange(String range) {
+
+    String lower = null;
+    String higher = null;
+    if (range == null) {
+      return prepareRange(lower, higher);
+    }
+
+
+    String doubleRange = "(-?[0-9]+\\.?[0-9]*)\\\\(-?[0-9]+\\.?[0-9]*)";
+    Matcher mdrange = getMatcher(doubleRange, range);
+    if (mdrange.matches()) {
+      lower = mdrange.group(1);
+      higher = mdrange.group(2);
+      return prepareRange(lower, higher);
+    }
+
+    String lowRange = "^<[=]? (-?[0-9]+\\.?[0-9]*)";
+    Matcher mlrange = getMatcher(lowRange, range);
+    if (mlrange.matches()) {
+      lower = mlrange.group(1);
+      return prepareRange(lower, higher);
+    }
+
+    String lowRangeBis = "(-?[0-9]+\\.?[0-9]*)\\\\";
+    Matcher mlrangebis = getMatcher(lowRangeBis, range);
+    if (mlrangebis.matches()) {
+      higher = mlrangebis.group(1);
+      return prepareRange(lower, higher);
+    }
+
+    String highRange = "^>[=]? (-?[0-9]+\\.?[0-9]*)";
+    Matcher mhrange = getMatcher(highRange, range);
+    if (mhrange.matches()) {
+      higher = mhrange.group(1);
+      return prepareRange(lower, higher);
+    }
+
+    String highRangeBis = "\\\\(-?[0-9]+\\.?[0-9]*)";
+    Matcher mhrangebis = getMatcher(highRangeBis, range);
+    if (mhrangebis.matches()) {
+      lower = mhrangebis.group(1);
+      return prepareRange(lower, higher);
+    }
+    String doubleRangeBis = "(-?[0-9]+\\.?[0-9]*)- \\+?([0-9]+\\.?[0-9]*)";
+    Matcher drangebis = getMatcher(doubleRangeBis, range);
+    if (drangebis.matches()) {
+      lower = drangebis.group(1);
+      higher = drangebis.group(2);
+      return prepareRange(lower, higher);
+    }
+    return prepareRange(lower, higher);
+  }
+
+  private Double[] prepareRange(String lower, String higher) {
+    Double lowerRst = null;
+    Double higherRst = null;
+    if (lower != null) {
+      lowerRst = Double.parseDouble(lower);
+    }
+    if (higher != null) {
+      higherRst = Double.parseDouble(higher);
+    }
+    Double[] rst = new Double[] {lowerRst, higherRst};
+
+    return rst;
+  }
+
+  private Matcher getMatcher(String pattern, String source) {
+    Pattern p = Pattern.compile(pattern);
+    Matcher m = p.matcher(source);
+    return m;
+  }
+
+  private String getBorne(Double value, Double low, Double high) {
+    String lowValue = "L";
+    String highValue = "H";
+    String normValue = "@";
+    if (low == null && high == null) {
+      return null;
+    }
+    if (low != null && high == null) {
+      if (value > low) {
+        return highValue;
+      } else {
+        return normValue;
+      }
+    }
+    if (low == null && high != null) {
+      if (value < high) {
+        return lowValue;
+      } else {
+        return normValue;
+      }
+    }
+    if (low != null && high != null) {
+      if (value <= low) {
+        return lowValue;
+      } else if (value >= high) {
+        return highValue;
+      } else {
+        return normValue;
+      }
+    }
+    return null;
+  }
+
   @Override
   public void process(Object[] records) throws HiveException {
     // need OI to convert data type to get java type
-    String value = ((StringObjectInspector) inputOIs[0]).getPrimitiveJavaObject(records[0]);
-    String range = ((StringObjectInspector) inputOIs[1]).getPrimitiveJavaObject(records[0]);
+    String range = null;
+    String source = null;
+    String value = null;
 
+    if (inputOIs[0] != null) {
+      value = ((StringObjectInspector) inputOIs[0]).getPrimitiveJavaObject(records[0]);
+    }
 
-    String type_calc = inferType(value);
-    Double value_num_calc  = null;
-    String unit_calc = null;
+    if (inputOIs[1] != null) {
+      range = ((StringObjectInspector) inputOIs[1]).getPrimitiveJavaObject(records[1]);
+    }
+
+    if (inputOIs[2] != null) {
+      source = ((StringObjectInspector) inputOIs[2]).getPrimitiveJavaObject(records[2]);
+    }
+
+    String value_type_calc = inferType(value);
+    Double value_num_calc = null;
     String value_text_calc = null;
-    
-    if ("datetime".equals(type_calc)) {
+    String value_num_unit_calc = null;
+    String value_num_operator_calc = null;
+    Double value_num_borne_inf_calc = null;
+    Double value_num_borne_sup_calc = null;
+    String value_num_borne_calc = null;
+
+
+    if ("datetime".equals(value_type_calc)) {
+      value_text_calc = value;
+    } else if ("numeric".equals(value_type_calc)) {
+      String[] spl = explodeNumericValue(value);
+      value_num_calc = Double.parseDouble(spl[0]);
+      value_num_unit_calc = spl[1];
+      Double[] rspl = explodeRange(range);
+      value_num_borne_inf_calc = rspl[0];
+      value_num_borne_sup_calc = rspl[1];
+      value_num_borne_calc =
+          getBorne(value_num_calc, value_num_borne_inf_calc, value_num_borne_sup_calc);
+
+    } else if ("image".equals(value_type_calc)) {
+      value_text_calc = value;
+    } else if ("gp_text".equals(value_type_calc)) {
+      value_text_calc = populateGptext(value, source);
+    } else { // text, xml, hour
       value_text_calc = value;
     }
-    if ("numeric".equals(type_calc)) {
-      String[] spl = value.split(" ");
-      value_num_calc= Double.parseDouble(spl[0]);
-      unit_calc = spl[1];
-    }
-    forwardColObj[0] = type_calc;
+    forwardColObj[0] = value_type_calc;
     forwardColObj[1] = value_num_calc;
-    forwardColObj[2] = unit_calc;
-    forwardColObj[3] = value_text_calc;
-    
+    forwardColObj[2] = value_text_calc;
+    forwardColObj[3] = value_num_unit_calc;
+    forwardColObj[4] = value_num_operator_calc;
+    forwardColObj[5] = value_num_borne_inf_calc;
+    forwardColObj[6] = value_num_borne_sup_calc;
+    forwardColObj[7] = value_num_borne_calc;
+
+
 
     // output a row with two column
     forward(forwardColObj);
+  }
+
+  private String[] explodeNumericValue(String value) {
+    Matcher a = this.typeNumericPattern.matcher(value);
+    a.matches();
+    return new String[]{a.group(1),a.group(2)};
+
   }
 
   @Override
--- src/test/java/fr/aphp/wind/hive/udf/UDFExtractGlimsAnalyseValueTest.java
+++ src/test/java/fr/aphp/wind/hive/udf/UDFExtractGlimsAnalyseValueTest.java
@@ -19,7 +19,7 @@ public class UDFExtractGlimsAnalyseValueTest {
 
   private UDFExtractGlimsAnalyseValue udf;
   private Object STRUCT =
-      "struct<type_calc:string,value_num_calc:double,unit_calc:string,value_text_calc:string>";
+      "struct<value_type_calc:string,value_num_calc:double,value_text_calc:string,value_num_unit_calc:string,value_num_operator_calc:string,value_num_borne_inf_calc:double,value_num_borne_sup_calc:double,value_num_borne_calc:string>";
 
   @Before
   public void before() {
@@ -35,14 +35,16 @@ public class UDFExtractGlimsAnalyseValueTest {
         out.add(input);
       }
     });
-    for (Object in : ins)
-      udtf.process(new Object[] {in});
+//    for (Object in : ins)
+      udtf.process(new Object[] {ins[0],ins[1],ins[2]});
     return out;
   }
 
 
 
   private static ConstantObjectInspector toConstantOI(final String text) {
+    if(text==null)
+      return null;
     return PrimitiveObjectInspectorFactory.getPrimitiveWritableConstantObjectInspector(
         TypeInfoFactory.stringTypeInfo, new Text(text));
   }
@@ -54,28 +56,176 @@ public class UDFExtractGlimsAnalyseValueTest {
   @Test
   public void howToManageNumeric() throws HiveException {
     final String VALUE = "9.9 mg/L";
-    final String RANGE = "9\10";
+    final String RANGE = "32.0\\36.0";
 
     final UDFExtractGlimsAnalyseValue sut = new UDFExtractGlimsAnalyseValue();
     final StructObjectInspector oi =
-        sut.initialize(new ObjectInspector[] {toConstantOI(VALUE), toConstantOI(RANGE)});
+        sut.initialize(new ObjectInspector[] {toConstantOI(VALUE), toConstantOI(RANGE), toConstantOI("apr")});
     assertEquals(
         this.STRUCT,
         oi.getTypeName());
 
 
-    final HivePath col1 = new HivePath(oi, ".type_calc");
+    final HivePath col1 = new HivePath(oi, ".value_type_calc");
     final HivePath col2 = new HivePath(oi, ".value_num_calc");
-    final HivePath col3 = new HivePath(oi, ".unit_calc");
-    final List<Object> results = evaluate(sut, toObject(VALUE));
+    final HivePath col3 = new HivePath(oi, ".value_num_unit_calc");
+    final HivePath col4 = new HivePath(oi, ".value_num_borne_inf_calc");
+    final HivePath col5 = new HivePath(oi, ".value_num_borne_sup_calc");
+    final HivePath col6 = new HivePath(oi, ".value_num_borne_calc");
+    final List<Object> results = evaluate(sut, toObject(VALUE),toObject(RANGE),toObject("apr"));
 
     assertEquals(1, results.size());
     assert ("numeric".equals(col1.extract(results.get(0)).asString()));
     assert (9.9 == col2.extract(results.get(0)).asDouble());
     assertEquals("mg/L", col3.extract(results.get(0)).asString());
+    assertEquals(32.0, col4.extract(results.get(0)).asDouble(),0);
+    assertEquals(36.0, col5.extract(results.get(0)).asDouble(),0);
+    assertEquals("L", col6.extract(results.get(0)).asString());
+    
 
   }
+  
+  @Test
+  public void shouldReturnHigh() throws HiveException {
+    final String VALUE = "40 mg/L";
+    final String RANGE = "32.0\\36.0";
 
+    final UDFExtractGlimsAnalyseValue sut = new UDFExtractGlimsAnalyseValue();
+    final StructObjectInspector oi =
+        sut.initialize(new ObjectInspector[] {toConstantOI(VALUE), toConstantOI(RANGE), toConstantOI("apr")});
+    assertEquals(
+        this.STRUCT,
+        oi.getTypeName());
+
+    final HivePath col6 = new HivePath(oi, ".value_num_borne_calc");
+    final List<Object> results = evaluate(sut, toObject(VALUE),toObject(RANGE),toObject("apr"));
+
+    assertEquals("H", col6.extract(results.get(0)).asString());
+  }
+  
+  @Test
+  public void shouldReturnLow() throws HiveException {
+    final String VALUE = "29 mg/L";
+    final String RANGE = ">= 32.0";
+
+    final UDFExtractGlimsAnalyseValue sut = new UDFExtractGlimsAnalyseValue();
+    final StructObjectInspector oi =
+        sut.initialize(new ObjectInspector[] {toConstantOI(VALUE), toConstantOI(RANGE), toConstantOI("apr")});
+    assertEquals(
+        this.STRUCT,
+        oi.getTypeName());
+
+    final HivePath col6 = new HivePath(oi, ".value_num_borne_calc");
+    final List<Object> results = evaluate(sut, toObject(VALUE),toObject(RANGE),toObject("apr"));
+
+    assertEquals("L", col6.extract(results.get(0)).asString());
+  }
+  
+  @Test
+  public void shouldReturnNorm() throws HiveException {
+    final String VALUE = "32 mg/L";
+    final String RANGE = ">= 32.0";
+
+    final UDFExtractGlimsAnalyseValue sut = new UDFExtractGlimsAnalyseValue();
+    final StructObjectInspector oi =
+        sut.initialize(new ObjectInspector[] {toConstantOI(VALUE), toConstantOI(RANGE), toConstantOI("apr")});
+    assertEquals(
+        this.STRUCT,
+        oi.getTypeName());
+
+    final HivePath col6 = new HivePath(oi, ".value_num_borne_calc");
+    final List<Object> results = evaluate(sut, toObject(VALUE),toObject(RANGE),toObject("apr"));
+
+    assertEquals("@", col6.extract(results.get(0)).asString());
+  }
+  
+  @Test
+  public void shouldReturnNormToo() throws HiveException {
+    final String VALUE = "32";
+    final String RANGE = ">= 32.0";
+
+    final UDFExtractGlimsAnalyseValue sut = new UDFExtractGlimsAnalyseValue();
+    final StructObjectInspector oi =
+        sut.initialize(new ObjectInspector[] {toConstantOI(VALUE), toConstantOI(RANGE), toConstantOI("apr")});
+    assertEquals(
+        this.STRUCT,
+        oi.getTypeName());
+
+    final HivePath col6 = new HivePath(oi, ".value_num_borne_calc");
+    final List<Object> results = evaluate(sut, toObject(VALUE),toObject(RANGE),toObject("apr"));
+
+    assertEquals(null, col6.extract(results.get(0)).asString());
+  }
+  @Test
+  public void shouldReturnVoid() throws HiveException {
+    final String VALUE = "32 mg/L";
+    final String RANGE = ">=32.0";//the range is not well defined then void
+
+    final UDFExtractGlimsAnalyseValue sut = new UDFExtractGlimsAnalyseValue();
+    final StructObjectInspector oi =
+        sut.initialize(new ObjectInspector[] {toConstantOI(VALUE), toConstantOI(RANGE), toConstantOI("apr")});
+    assertEquals(
+        this.STRUCT,
+        oi.getTypeName());
+
+    final HivePath col6 = new HivePath(oi, ".value_num_borne_calc");
+    final List<Object> results = evaluate(sut, toObject(VALUE),toObject(RANGE),toObject("apr"));
+
+    assertEquals(null, col6.extract(results.get(0)).asString());
+  }
+  
+  @Test
+  public void shouldReturnVoidToo() throws HiveException {
+    final String VALUE = null;
+    final String RANGE = null;//the range is not well defined then void
+
+    final UDFExtractGlimsAnalyseValue sut = new UDFExtractGlimsAnalyseValue();
+    final StructObjectInspector oi =
+        sut.initialize(new ObjectInspector[] {toConstantOI(VALUE), toConstantOI(RANGE), toConstantOI(null)});
+    assertEquals(
+        this.STRUCT,
+        oi.getTypeName());
+
+    final HivePath col6 = new HivePath(oi, ".value_num_borne_calc");
+    final List<Object> results = evaluate(sut, toObject(VALUE),toObject(RANGE),toObject(null));
+
+    assertEquals(null, col6.extract(results.get(0)).asString());
+  }
+  
+  @Test
+  public void shouldReturnVoidTooToo() throws HiveException {
+    final String VALUE = "";
+    final String RANGE = "";//the range is not well defined then void
+
+    final UDFExtractGlimsAnalyseValue sut = new UDFExtractGlimsAnalyseValue();
+    final StructObjectInspector oi =
+        sut.initialize(new ObjectInspector[] {toConstantOI(VALUE), toConstantOI(RANGE), toConstantOI("")});
+    assertEquals(
+        this.STRUCT,
+        oi.getTypeName());
+
+    final HivePath col6 = new HivePath(oi, ".value_num_borne_calc");
+    final List<Object> results = evaluate(sut, toObject(VALUE),toObject(RANGE),toObject(""));
+
+    assertEquals(null, col6.extract(results.get(0)).asString());
+  }
+  @Test
+  public void shouldReturnSomething() throws HiveException {
+    final String VALUE = "< 0.04 µg/l";
+    final String RANGE = "\\0.15";//the range is not well defined then void
+
+    final UDFExtractGlimsAnalyseValue sut = new UDFExtractGlimsAnalyseValue();
+    final StructObjectInspector oi =
+        sut.initialize(new ObjectInspector[] {toConstantOI(VALUE), toConstantOI(RANGE), toConstantOI("")});
+    assertEquals(
+        this.STRUCT,
+        oi.getTypeName());
+
+    final HivePath col6 = new HivePath(oi, ".value_num_borne_calc");
+    final List<Object> results = evaluate(sut, toObject(VALUE),toObject(RANGE),toObject(""));
+
+    assertEquals("@", col6.extract(results.get(0)).asString());
+  }
   @Test
   public void howToManageDatetime() throws HiveException {
     final String VALUE = "01/01/1988 13:24";
@@ -83,21 +233,83 @@ public class UDFExtractGlimsAnalyseValueTest {
 
     final UDFExtractGlimsAnalyseValue sut = new UDFExtractGlimsAnalyseValue();
     final StructObjectInspector oi =
-        sut.initialize(new ObjectInspector[] {toConstantOI(VALUE), toConstantOI(RANGE)});
+        sut.initialize(new ObjectInspector[] {toConstantOI(VALUE), toConstantOI(RANGE), toConstantOI("apr")});
     assertEquals(this.STRUCT, oi.getTypeName());
 
 
-    final HivePath col1 = new HivePath(oi, ".type_calc");
+    final HivePath col1 = new HivePath(oi, ".value_type_calc");
     final HivePath col2 = new HivePath(oi, ".value_num_calc");
-    final HivePath col3 = new HivePath(oi, ".unit_calc");
+    final HivePath col3 = new HivePath(oi, ".value_num_unit_calc");
     final HivePath col4 = new HivePath(oi, ".value_text_calc");
-    final List<Object> results = evaluate(sut, toObject(VALUE));
+    final List<Object> results = evaluate(sut, toObject(VALUE), toObject(RANGE), toObject("apr"));
 
     assertEquals(1, results.size());
     assert ("datetime".equals(col1.extract(results.get(0)).asString()));
-    // assertEquals(col2.extract(results.get(0)).asDouble(), null);
+  //  assert(Double.is(col2.extract(results.get(0)).asDouble()));
     assertEquals(null, col3.extract(results.get(0)).asString());
     assertEquals(VALUE, col4.extract(results.get(0)).asString());
 
   }
+  
+  @Test
+  public void howToManageImg() throws HiveException {
+    final String VALUE = "house.TIF";
+    final String RANGE = null;
+
+    final UDFExtractGlimsAnalyseValue sut = new UDFExtractGlimsAnalyseValue();
+    final StructObjectInspector oi =
+        sut.initialize(new ObjectInspector[] {toConstantOI(VALUE), toConstantOI(RANGE), toConstantOI("apr")});
+    assertEquals(this.STRUCT, oi.getTypeName());
+
+
+    final HivePath col1 = new HivePath(oi, ".value_type_calc");
+    final HivePath col4 = new HivePath(oi, ".value_text_calc");
+    final List<Object> results = evaluate(sut, toObject(VALUE), toObject(RANGE), toObject("apr"));
+
+    assertEquals(1, results.size());
+    assert ("image".equals(col1.extract(results.get(0)).asString()));
+  //  assert(Double.is(col2.extract(results.get(0)).asDouble()));
+    assertEquals(VALUE, col4.extract(results.get(0)).asString());
+  }
+  
+  @Test
+  public void shouldNotReturnImage() throws HiveException {
+    final String VALUE = "house.TIFO";
+    final String RANGE = null;
+
+    final UDFExtractGlimsAnalyseValue sut = new UDFExtractGlimsAnalyseValue();
+    final StructObjectInspector oi =
+        sut.initialize(new ObjectInspector[] {toConstantOI(VALUE), toConstantOI(RANGE), toConstantOI("apr")});
+    assertEquals(this.STRUCT, oi.getTypeName());
+
+
+    final HivePath col1 = new HivePath(oi, ".value_type_calc");
+    final HivePath col4 = new HivePath(oi, ".value_text_calc");
+    final List<Object> results = evaluate(sut, toObject(VALUE), toObject(RANGE), toObject("apr"));
+
+    assertEquals(1, results.size());
+    assert ("text".equals(col1.extract(results.get(0)).asString()));
+    assertEquals(VALUE, col4.extract(results.get(0)).asString());
+  }
+  
+  @Test
+  public void howToManageGptext() throws HiveException {
+    final String VALUE = "hello {<_rpcCOL11A1} world!";
+    final String RANGE = null;
+
+    final UDFExtractGlimsAnalyseValue sut = new UDFExtractGlimsAnalyseValue();
+    final StructObjectInspector oi =
+        sut.initialize(new ObjectInspector[] {toConstantOI(VALUE), toConstantOI(RANGE), toConstantOI("apr")});
+    assertEquals(this.STRUCT, oi.getTypeName());
+
+
+    final HivePath col1 = new HivePath(oi, ".value_type_calc");
+    final HivePath col4 = new HivePath(oi, ".value_text_calc");
+    final List<Object> results = evaluate(sut, toObject(VALUE), toObject(RANGE), toObject("apr"));
+
+    assertEquals(1, results.size());
+    assert ("gp_text".equals(col1.extract(results.get(0)).asString()));
+    assertEquals("hello COL11A1 world!", col4.extract(results.get(0)).asString());
+  }
+  
 }
