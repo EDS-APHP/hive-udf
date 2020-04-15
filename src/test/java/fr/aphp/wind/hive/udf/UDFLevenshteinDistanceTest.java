9596ead9806ea23f18b2ce5e20c0152f15432727
tlama <tristan.lama@gmail.com>
2020-04-15 17:01:05 +0200

    Add UDFLevenshteinDistanceTest

---
+++ src/test/java/fr/aphp/wind/hive/udf/UDFLevenshteinDistanceTest.java
@@ -0,0 +1,23 @@
+package fr.aphp.wind.hive.udf;
+
+
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UDFLevenshteinDistanceTest {
+
+    private UDFLevenshteinDistance udf;
+
+    @Before
+    public void before() {
+        udf = new UDFLevenshteinDistance();
+    }
+
+    @Test
+    public void computeDistance(){
+        String c1 = "azerty";
+        Assert.assertEquals((double) 0,udf.evaluate(c1,c1));
+        Assert.assertEquals((double) c1.length(),udf.evaluate(c1,c1.concat(c1)));
+    }
+}
