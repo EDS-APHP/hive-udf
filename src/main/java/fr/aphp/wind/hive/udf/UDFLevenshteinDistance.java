6361f899ab6907a1ba36eb30e0d07c1927d2b244
tlama_cap <tristan.lama@capgemini.com>
2020-04-19 19:53:30 +0200

    Change extends from UDF into GenericUDF to avoid multiple StringDistance initialisation

--- src/main/java/fr/aphp/wind/hive/udf/UDFLevenshteinDistance.java
+++ src/main/java/fr/aphp/wind/hive/udf/UDFLevenshteinDistance.java
@@ -3,24 +3,82 @@ package fr.aphp.wind.hive.udf;
 import info.debatty.java.stringsimilarity.CharacterSubstitutionInterface;
 import info.debatty.java.stringsimilarity.WeightedLevenshtein;
 import org.apache.hadoop.hive.ql.exec.Description;
-import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.PrimitiveObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
 
 import java.text.Normalizer;
 
 @Description(name = "levenshtein_distance", value = "double _FUNC_(String c1, String c2) - get the Levenshtein distance between two strings")
-public class UDFLevenshteinDistance extends UDF {
+public class UDFLevenshteinDistance extends GenericUDF {
+
+    private StringDistance stringDistance;
+    private PrimitiveObjectInspector inputsObj;
+    private PrimitiveObjectInspector outputObj;
+
+
+    @Override
+    public ObjectInspector initialize(ObjectInspector[] args) throws UDFArgumentException {
+
+        stringDistance = new StringDistance();
+
+        boolean areInputsAsString = true;
+        //Check that we receive 2 arguments
+        if (args.length != 2) {
+            throw new UDFArgumentException(" Expecting 2 arguments ");
+        }
+
+        for (ObjectInspector arg : args) {
+            areInputsAsString &= arg.getCategory() == ObjectInspector.Category.PRIMITIVE;
+            if (areInputsAsString) {
+                inputsObj = (PrimitiveObjectInspector) arg;
+                areInputsAsString = inputsObj.getPrimitiveCategory() == PrimitiveObjectInspector.PrimitiveCategory.STRING;
+            }
+        }
+
+        //Check that we receive 2 String arguments
+        if (!areInputsAsString) {
+            throw new UDFArgumentException(" Expecting all arguments to be String ");
+        }
+
+        outputObj = PrimitiveObjectInspectorFactory.writableDoubleObjectInspector;
+        return outputObj;
+    }
+
+    @Override
+    public Object evaluate(DeferredObject[] args) throws HiveException {
+
+        String c1, c2;
+
+        if (args.length == 2 && args[0] != null && args[1] != null) {
+
+            c1 = (String) inputsObj.getPrimitiveJavaObject(args[0].get());
+            c2 = (String) inputsObj.getPrimitiveJavaObject(args[1].get());
+        } else {
+            return null;
+        }
+        return outputObj.getPrimitiveWritableObject(stringDistance.distance(c1, c2));
+    }
+
+    @Override
+    public String getDisplayString(String[] children) {
+        return "return_double = levenshtein_distance( '"+children[0]+"' , '"+children[1]+"' )";
+    }
 
     /**
      * Calculates the Levenshtein distance between 2 strings
      */
-    public double evaluate(String c1, String c2){
-        return new StringDistance().distance(c1,c2);
-    }
+//    public double evaluate(String c1, String c2){
+//        return stringDistance.distance(c1,c2);
+//    }
 
-    private class StringDistance{
+    private class StringDistance {
         WeightedLevenshtein distKernel;
 
-    public StringDistance(){
+        public StringDistance() {
             distKernel = new WeightedLevenshtein(new CharacterSubstitutionInterface() {
                 public double cost(char c1, char c2) {
 
@@ -171,11 +229,11 @@ public class UDFLevenshteinDistance extends UDF {
         }
 
         private boolean isSimilar(String original, String candidate, boolean normAccent) {
-            if(normAccent){
+            if (normAccent) {
                 original = stripAccents(original);
                 candidate = stripAccents(candidate);
             }
-            if( original.length() != candidate.length() ){
+            if (original.length() != candidate.length()) {
                 return false;
             }
             double seuil;
@@ -187,7 +245,7 @@ public class UDFLevenshteinDistance extends UDF {
             } else { // petit mot
                 seuil = original.length() / 3 * 0.5;
             }
-            if(original.length() > 5){// on substitue avant de calculer la distance
+            if (original.length() > 5) {// on substitue avant de calculer la distance
                 ReorganizeString reor;
                 reor = new ReorganizeString(candidate, original);
                 reor.process();
@@ -221,41 +279,43 @@ public class UDFLevenshteinDistance extends UDF {
                 this.goal = goal;
             }
 
-            public void process(){
+            public void process() {
                 Integer step = 0;
-                while(true){
-                    if(getWindOriginal(step).equals(getWindGoal(step))){
+                while (true) {
+                    if (getWindOriginal(step).equals(getWindGoal(step))) {
                         //les morceaux sont égaux
-                    }else if(substitute(getWindGoal(step)).equals(getWindOriginal(step))){//une substitution rétablit...
-                        original =  transform(step, original, getWindGoal(step));
+                    } else if (substitute(getWindGoal(step)).equals(getWindOriginal(step))) {//une substitution rétablit...
+                        original = transform(step, original, getWindGoal(step));
                         substitutionNumber++;
                     }
                     step++;//on avance dans le cadre
-                    if(step + 1 >= original.length() ){break;}//la fin approche
+                    if (step + 1 >= original.length()) {
+                        break;
+                    }//la fin approche
                 }
             }
 
-            private String getWindOriginal(final Integer i){
-                return this.original.substring(i, i+2);
+            private String getWindOriginal(final Integer i) {
+                return this.original.substring(i, i + 2);
             }
 
-            private String getWindGoal(final Integer i){
-                return this.goal.substring(i, i+2);
+            private String getWindGoal(final Integer i) {
+                return this.goal.substring(i, i + 2);
             }
 
-            private String substitute(String db){
+            private String substitute(String db) {
                 return String.format("%s%s", db.charAt(1), db.charAt(0));
             }
 
-            private String transform(Integer step, String origin, String replacement){
-                return origin.substring(0,step) + replacement + origin.substring(step+2, origin.length());
+            private String transform(Integer step, String origin, String replacement) {
+                return origin.substring(0, step) + replacement + origin.substring(step + 2, origin.length());
             }
 
-            public Integer getSubstitutionNumber(){
+            public Integer getSubstitutionNumber() {
                 return this.substitutionNumber;
             }
 
-            public String getReorganizedString(){
+            public String getReorganizedString() {
                 return this.original;
             }
         }
